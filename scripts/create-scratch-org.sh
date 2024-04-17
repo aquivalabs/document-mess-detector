9382962edde0271731d4f9d6227b2ea9091dd014
Robert Sösemann <robert@soesemann.com>
2024-04-17 12:06:48 +0200

    Prepared builders for Code Live demos

--- scripts/create-scratch-org.sh
+++ scripts/create-scratch-org.sh
@@ -15,14 +15,14 @@ sf org delete scratch --no-prompt --target-org $SCRATCH_ORG_ALIAS
 echo "Creating scratch org"
 execute sf org create scratch --alias $SCRATCH_ORG_ALIAS --set-default --definition-file ./config/project-scratch-def.json --duration-days 30
 
+echo "Make sure Org user is english"
+sf data update record --sobject User --where "Name='User User'" --values "Languagelocalekey=en_US"
+
 echo "Pushing changes to scratch org"
 execute sf project deploy start
 
 echo "Assigning permissions"
 execute sf force user permset assign --perm-set-name AppAdministrator --perm-set-name DocumentReviewer --perm-set-name RuleAuthor
-
-echo "Make sure Org user is english"
-sf data update record --sobject User --where "Name='User User'" --values "Languagelocalekey=en_US"
 
 echo "Running Apex Tests"
 execute sf apex run test --test-level RunLocalTests --code-coverage --result-format human --synchronous
